a31c0a0a5d57d980c7e816deedaf30fb5fed9ca5
yagisuke <yagisuke.0209@gmail.com>
2020-05-05 02:03:35 +0900

    fix

--- test/ava.test.js
+++ test/ava.test.js
@@ -1,13 +1,9 @@
-import test from 'ava'
-import { Nuxt, Builder } from 'nuxt'
-import { resolve } from 'path'
-
-// Nuxt への参照を保持します
-// そうすればテスト終了時にサーバーをクローズできます
-let nuxt = null
+const { resolve } = require('path')
+const test = require('ava')
+const { Nuxt, Builder } = require('nuxt')
 
 // Nuxt.js を初期化し localhost:4000 のリスニングを開始します
-test.before('Init Nuxt.js', async () => {
+test.before('Init Nuxt.js', async (t) => {
   const rootDir = resolve(__dirname, '..')
   let config = {}
   try {
@@ -18,20 +14,24 @@ test.before('Init Nuxt.js', async () => {
   config.rootDir = rootDir // project folder
   config.dev = false // production build
   config.mode = 'universal' // Isomorphic application
-  nuxt = new Nuxt(config)
+  const nuxt = new Nuxt(config)
+  t.context.nuxt = nuxt // Nuxt への参照を保持することでテスト終了時にサーバーをクローズすることができます
   await new Builder(nuxt).build()
   nuxt.listen(4000, 'localhost')
 })
 
 // 生成された HTML のみをテストする例
-test('Route / exits and render HTML', async (t) => {
-  let context = {}
+test('Route / exists and render HTML', async (t) => {
+  const { nuxt } = t.context
+  const context = {}
   const { html } = await nuxt.renderRoute('/ava', context)
+  console.warn('fffffff', html)
   t.true(html.includes('<h1 class="red">Hello world!</h1>'))
 })
 
 // DOM チェックを経由してテストする例
-test('Route / exits and render HTML with CSS applied', async (t) => {
+test('Route / exists and renders HTML with CSS applied', async (t) => {
+  const { nuxt } = t.context
   const window = await nuxt.renderAndGetWindow('http://localhost:4000/ava')
   const element = window.document.querySelector('.red')
   t.not(element, null)
@@ -41,6 +41,7 @@ test('Route / exits and render HTML with CSS applied', async (t) => {
 })
 
 // Nuxt サーバーをクローズする
-test.after('Closing server', () => {
+test.after('Closing server', (t) => {
+  const { nuxt } = t.context
   nuxt.close()
 })
